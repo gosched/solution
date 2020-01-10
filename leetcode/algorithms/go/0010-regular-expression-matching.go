2386aa0e26e6d6466e9cc45bf4d06956a9f24c53
gosched <45745902+gosched@users.noreply.github.com>
2020-01-10 11:11:56 +0800

    Create 0010-regular-expression-matching.go

---
+++ leetcode/algorithms/go/0010-regular-expression-matching.go
@@ -0,0 +1,23 @@
+package leetcode
+
+func isMatch(s string, p string) bool {
+	if p == "" {
+		return s == ""
+	}
+	if 1 < len(p) && p[1] == '*' {
+		return isMatch(s, p[2:]) || s != "" && (s[0] == p[0] || p[0] == '.') && isMatch(s[1:], p)
+	}
+	return s != "" && (s[0] == p[0] || p[0] == '.') && isMatch(s[1:], p[1:])
+}
+
+// func isMatch(s string, p string) bool {
+// 	matched, _ := regexp.MatchString("^"+p+"$", s)
+// 	return matched
+// }
+
+// func isMatch(s string, p string) bool {
+// 	re := regexp.MustCompile("^" + p + "$")
+// 	return re.MatchString(s)
+// }
+
+// https://golang.org/pkg/regexp/
