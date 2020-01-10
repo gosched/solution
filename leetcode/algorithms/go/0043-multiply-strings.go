eb72d903aca34c1ce0516b3721fd5390d3c29ffa
gosched <45745902+gosched@users.noreply.github.com>
2020-01-10 11:14:57 +0800

    Create 0043-multiply-strings.go

---
+++ leetcode/algorithms/go/0043-multiply-strings.go
@@ -0,0 +1,15 @@
+package leetcode
+
+// import "math/big"
+
+// func multiply(num1 string, num2 string) string {
+// 	n1 := new(big.Int)
+// 	n2 := new(big.Int)
+
+// 	n1, _ = n1.SetString(num1, 10)
+// 	n2, _ = n2.SetString(num2, 10)
+
+// 	n1 = n1.Mul(n1, n2)
+
+// 	return n1.String()
+// }
