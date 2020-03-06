c6f6bafb321ed149df8e768885787df7146b8397
gosched <45745902+gosched@users.noreply.github.com>
2020-03-06 18:12:35 +0800

    Update 0005-longest-palindromic-substring.go

--- leetcode/algorithms/go/0005-longest-palindromic-substring.go
+++ leetcode/algorithms/go/0005-longest-palindromic-substring.go
@@ -1,24 +1,40 @@
 package leetcode
 
 func longestPalindrome(s string) string {
-	var start, length = 0, 0
-
-	helper := func(i, j int, start, length *int) {
+	var start, length int
+	var helper = func(i, j int, start, length *int) {
 		for -1 < i && j < len(s) && s[i] == s[j] {
 			i--
 			j++
 		}
-
 		if *length < j-i-1 {
 			*start = i + 1
 			*length = j - i - 1
 		}
 	}
-
 	for i := range s {
 		helper(i, i, &start, &length)
 		helper(i, i+1, &start, &length)
 	}
-
 	return s[start : start+length]
 }
+
+// func longestPalindrome(s string) string {
+// 	var start, length int
+// 	var helper = func(i, j, start, length int) (int, int) {
+// 		for -1 < i && j < len(s) && s[i] == s[j] {
+// 			i--
+// 			j++
+// 		}
+// 		if length < j-i-1 {
+// 			start = i + 1
+// 			length = j - i - 1
+// 		}
+// 		return start, length
+// 	}
+// 	for i := range s {
+// 		start, length = helper(i, i, start, length)
+// 		start, length = helper(i, i+1, start, length)
+// 	}
+// 	return s[start : start+length]
+// }
