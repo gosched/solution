a0dee7cd538f832f7e4f858ee5c17f87579ab656
gosched <45745902+gosched@users.noreply.github.com>
2020-03-06 18:30:28 +0800

    Update 0015-3sum.go

--- leetcode/algorithms/go/0015-3sum.go
+++ leetcode/algorithms/go/0015-3sum.go
@@ -3,26 +3,26 @@ package leetcode
 import "sort"
 
 func threeSum(nums []int) [][]int {
-	result := [][]int{}
+	set := [][]int{}
 	length := len(nums)
-	limit := length - 2
 	sort.Ints(nums)
-	for i := 0; i < limit; i++ {
+	for i := 0; i < length-2; i++ {
+		a := nums[i]
+		if 0 < a {
+			break
+		}
 		if 0 < i && nums[i] == nums[i-1] {
 			continue
 		}
-		indexB := i + 1
-		indexC := length - 1
+		indexB, indexC := i+1, length-1
 		for indexB < indexC {
-			a := nums[i]
-			b := nums[indexB]
-			c := nums[indexC]
+			b, c := nums[indexB], nums[indexC]
 			if a+b+c < 0 {
 				indexB++
 			} else if a+b+c > 0 {
 				indexC--
 			} else {
-				result = append(result, []int{a, b, c})
+				set = append(set, []int{a, b, c})
 				for indexB < indexC && nums[indexB] == nums[indexB+1] {
 					indexB++
 				}
@@ -34,5 +34,5 @@ func threeSum(nums []int) [][]int {
 			}
 		}
 	}
-	return result
+	return set
 }
