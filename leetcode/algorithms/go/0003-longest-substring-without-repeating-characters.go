56fb0ca4b3c0a9b0a1fceeed8ca86aabbace5d2e
gosched <45745902+gosched@users.noreply.github.com>
2020-03-23 23:38:45 +0800

    Update 0003-longest-substring-without-repeating-characters.go

--- leetcode/algorithms/go/0003-longest-substring-without-repeating-characters.go
+++ leetcode/algorithms/go/0003-longest-substring-without-repeating-characters.go
@@ -1,19 +1,19 @@
 package leetcode
 
 func lengthOfLongestSubstring(s string) int {
-    var length = 0
-    var beforeStart = -1
-    var m = make(map[rune]int)
-    for i, r := range s {
-        if j, exist := m[r]; exist && beforeStart < j {
-            beforeStart = j
-        }
-        m[r] = i
-        if length < i-beforeStart {
-            length = i - beforeStart
-        }
-    }
-    return length
+	var length = 0
+	var beforeStart = -1
+	var m = make(map[rune]int)
+	for i, r := range s {
+		if j, exist := m[r]; exist && beforeStart < j {
+			beforeStart = j
+		}
+		m[r] = i
+		if length < i-beforeStart {
+			length = i - beforeStart
+		}
+	}
+	return length
 }
 
 // beforeStart == 當前子字串的前一個索引
